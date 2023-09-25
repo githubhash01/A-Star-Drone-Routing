0bd95b8abbdc91d086badb1e9ee78a04b610d1df
Hashim Al-Obaidi <hashimao@icloud.com>
2023-09-25 19:36:48 +0100

    First commit

---
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -0,0 +1,18 @@
+package uk.ac.ed.inf;
+// importing the CentralRegionVertexOrder class
+import uk.ac.ed.inf.ilp.constant.CentralRegionVertexOrder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        // testing a property from CentralRegionVertexOrder utility class
+        System.out.println(CentralRegionVertexOrder.TOP_LEFT);
+    }
+}
---
+++ src/main/java/uk/ac/ed/inf/LngLatHandler.java
@@ -0,0 +1,8 @@
+package uk.ac.ed.inf;
+
+/*
+A class that implements the LngLatHandler interface
+ */
+
+public class LngLatHandler {
+}
