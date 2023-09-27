7e85788a0bca8f773e2212cbfe677977d68a4929
Hashim Al-Obaidi <hashimao@icloud.com>
2023-09-27 20:39:09 +0100

    First half of order validation + tests

--- src/main/java/uk/ac/ed/inf/OrderValidator.java
+++ src/main/java/uk/ac/ed/inf/OrderValidator.java
@@ -4,11 +4,113 @@ package uk.ac.ed.inf;
 import uk.ac.ed.inf.ilp.data.Order;
 import uk.ac.ed.inf.ilp.data.Restaurant;
 import uk.ac.ed.inf.ilp.interfaces.OrderValidation;
+import uk.ac.ed.inf.ilp.constant.OrderValidationCode;
+import uk.ac.ed.inf.ilp.data.Pizza;
+// import system constants
+import uk.ac.ed.inf.ilp.constant.SystemConstants;
+
 
 public class OrderValidator implements OrderValidation {
     @Override
     public Order validateOrder(Order orderToValidate, Restaurant[] definedRestaurants) {
+        // 1. Card Number
+        String cardNumber = orderToValidate.getCreditCardInformation().getCreditCardNumber();
+        if (!validCardNumber(cardNumber)) {
+            // update the order validation status
+            orderToValidate.setOrderValidationCode(OrderValidationCode.CARD_NUMBER_INVALID);
+            return orderToValidate;
+        }
 
-        return null;
+        // 2. Expiry date
+        String orderExpiryDate = orderToValidate.getCreditCardInformation().getCreditCardExpiry();
+        String orderDate = orderToValidate.getOrderDate().toString();
+        if (!validExpiry(orderExpiryDate, orderDate)) {
+            // update the order validation status
+            orderToValidate.setOrderValidationCode(OrderValidationCode.EXPIRY_DATE_INVALID);
+            return orderToValidate;
+        }
+
+        // 3. CVV Number
+        String CVV = orderToValidate.getCreditCardInformation().getCvv();
+        if (!validCVV(CVV)) {
+            // update the order validation status
+            orderToValidate.setOrderValidationCode(OrderValidationCode.CVV_INVALID);
+            return orderToValidate;
+        }
+
+        // 4. Total Price
+        Pizza[] pizzas = orderToValidate.getPizzasInOrder();
+        int priceTotalInPence = orderToValidate.getPriceTotalInPence();
+        if (!correctTotalPrice(pizzas, priceTotalInPence)) {
+            // update the order validation status
+            orderToValidate.setOrderValidationCode(OrderValidationCode.TOTAL_INCORRECT);
+            return orderToValidate;
+        }
+        // 5. Defined Pizzas
+
+        // 6. Excessive Pizza
+
+        // 7. Multiple Restaurants
+
+        // 8. Restaurant Opening Hours
+
+
+        // update the order validation status
+        orderToValidate.setOrderValidationCode(OrderValidationCode.NO_ERROR);
+        return orderToValidate;
+    }
+
+    public boolean validCardNumber(String cardNumber) {
+        // check if the cardNumber is length 16
+        if (cardNumber.length() != 16) {
+            return false;
+        }
+        // check if any characters in the cardNumber are not digits
+        for (int i = 0; i < cardNumber.length(); i++) {
+            if (!Character.isDigit(cardNumber.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean validExpiry(String expiryDate, String orderDate) {
+        // check if the expiry date occurs or on the order date
+        // orderDate is in YYYY-MM-DD format whereas expiryDate is in MM/YY format so parsing necessary
+        String[] orderDateSplit = orderDate.split("-");
+        int orderYear = Integer.parseInt(orderDateSplit[0]);
+        int orderMonth = Integer.parseInt(orderDateSplit[1]);
+        // extract the month and year from expiryDate
+        String[] expiryDateSplit = expiryDate.split("/");
+        int expiryYear = 2000 + Integer.parseInt(expiryDateSplit[1]); // add 2000 to the year
+        int expiryMonth = Integer.parseInt(expiryDateSplit[0]);
+        // check if the expiry date is not before or on the order date
+        return expiryYear >= orderYear && (expiryYear != orderYear || expiryMonth > orderMonth);
+    }
+
+    public boolean validCVV(String CVV){
+        // check if the CVV is length 3
+        if (CVV.length() != 3) {
+            return false;
+        }
+        // check if any characters in the CVV are not digits
+        for (int i = 0; i < CVV.length(); i++) {
+            if (!Character.isDigit(CVV.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean correctTotalPrice(Pizza[] pizzas,  int priceTotalInPence){
+        int calculatedTotal = 0;
+        // iterate through the pizzas and add up the prices
+        for (Pizza pizza : pizzas) {
+            calculatedTotal += pizza.priceInPence();
+        }
+        // add the delivery charge form the system constants
+        calculatedTotal += SystemConstants.ORDER_CHARGE_IN_PENCE;
+        // check if the total price is equal to priceTotalInPence
+        return calculatedTotal == priceTotalInPence;
     }
 }
--- src/test/java/uk/ac/ed/inf/OrderValidatorTest.java
+++ src/test/java/uk/ac/ed/inf/OrderValidatorTest.java
@@ -6,6 +6,12 @@ import junit.framework.TestSuite;
 
 // importing order
 import uk.ac.ed.inf.ilp.data.Order;
+import uk.ac.ed.inf.ilp.data.CreditCardInformation;
+import uk.ac.ed.inf.ilp.constant.OrderValidationCode;
+import uk.ac.ed.inf.ilp.constant.SystemConstants;
+import uk.ac.ed.inf.ilp.data.Pizza;
+
+import java.time.LocalDate;
 
 public class OrderValidatorTest
         extends TestCase
@@ -28,25 +34,130 @@ public class OrderValidatorTest
         return new TestSuite( OrderValidatorTest.class );
     }
 
-    // creating an order object
-    Order order = new Order();
-
     // testing OrderValidationCode possible errors
 
     public void testingCardNumberInvalid(){
-        // testing card number is valid
-        // card number must be a string containing only digits and be 16 digits long (no spaces)
+        // card number must be a string, with exactly 16 digits
+        Order orderCardNumberShort = new Order();
+        orderCardNumberShort.setCreditCardInformation(
+                new CreditCardInformation(
+                        "1212",
+                        "12/12",
+                        "222"
+                )
+        );
+        OrderValidator orderValidator = new OrderValidator();
+        // order validator returns an order with a defined order validation code
+        Order validatedOrder = orderValidator.validateOrder(orderCardNumberShort, null);
+
+        // assert that the order validation code is CARD_NUMBER_INVALID
+        assertEquals(OrderValidationCode.CARD_NUMBER_INVALID, validatedOrder.getOrderValidationCode());
+
+        // card number too long
+        Order orderCardNumberLong = new Order();
+        orderCardNumberLong.setCreditCardInformation(
+                new CreditCardInformation(
+                        "12121212121212121",
+                        "12/12",
+                        "222"
+                )
+        );
+        OrderValidator orderValidator2 = new OrderValidator();
+        // order validator returns an order with a defined order validation code
+        Order validatedOrder2 = orderValidator2.validateOrder(orderCardNumberLong, null);
+        assertEquals(OrderValidationCode.CARD_NUMBER_INVALID, validatedOrder2.getOrderValidationCode());
+
+        // card number is not entirely digits
+        Order orderCardNumberAlpha = new Order();
+        orderCardNumberAlpha.setCreditCardInformation(
+                new CreditCardInformation(
+                        "1212a121212121212",
+                        "12/12",
+                        "222"
+                )
+        );
+        OrderValidator orderValidator3 = new OrderValidator();
+        // order validator returns an order with a defined order validation code
+        Order validatedOrder3 = orderValidator3.validateOrder(orderCardNumberAlpha, null);
+        assertEquals(OrderValidationCode.CARD_NUMBER_INVALID, validatedOrder3.getOrderValidationCode());
 
     }
     public void testingExpiryDateInvalid(){
+        // creating an order with an expiry date before the order date
+        Order orderExpiryBeforeOrder = new Order();
+        orderExpiryBeforeOrder.setOrderDate(LocalDate.of(2023, 9, 1));
+        orderExpiryBeforeOrder.setCreditCardInformation(
+                new CreditCardInformation(
+                        "1212121212121212",
+                        "12/22",
+                        "222"
+                )
+        );
+        OrderValidator orderValidator = new OrderValidator();
+        // order validator returns an order with a defined order validation code
+        Order validatedOrder = orderValidator.validateOrder(orderExpiryBeforeOrder, null);
+        assertEquals(OrderValidationCode.EXPIRY_DATE_INVALID, validatedOrder.getOrderValidationCode());
 
+        // testing an expiry date in the same month as the order date, but before the order date
+        Order orderExpirySameMonthBeforeOrder = new Order();
+        orderExpirySameMonthBeforeOrder.setOrderDate(LocalDate.of(2023, 9, 1));
+        orderExpirySameMonthBeforeOrder.setCreditCardInformation(
+                new CreditCardInformation(
+                        "1212121212121212",
+                        "09/23",
+                        "222"
+                )
+        );
+        OrderValidator orderValidator2 = new OrderValidator();
+        // order validator returns an order with a defined order validation code
+        Order validatedOrder2 = orderValidator2.validateOrder(orderExpirySameMonthBeforeOrder, null);
+        assertEquals(OrderValidationCode.EXPIRY_DATE_INVALID, validatedOrder2.getOrderValidationCode());
     }
 
     public void testingCVVInvalid(){
-
+        // making an order with CVV that is invalid
+        Order orderCVVInvalid = new Order();
+        orderCVVInvalid.setOrderDate(LocalDate.of(2023, 9, 1));
+        orderCVVInvalid.setCreditCardInformation(
+                new CreditCardInformation(
+                        "1212121212121212",
+                        "09/28",
+                        "22"
+                )
+        );
+        OrderValidator orderValidator = new OrderValidator();
+        // order validator returns an order with a defined order validation code
+        Order validatedOrder = orderValidator.validateOrder(orderCVVInvalid, null);
+        assertEquals(OrderValidationCode.CVV_INVALID, validatedOrder.getOrderValidationCode());
     }
 
     public void testingTotalIncorrect(){
+        // making sure the total is greater than deliver fee
+        // generating order
+        Order orderTotalIncorrect = new Order();
+        orderTotalIncorrect.setOrderDate(LocalDate.of(2023, 9, 1));
+        orderTotalIncorrect.setCreditCardInformation(
+                new CreditCardInformation(
+                        "1212121212121212",
+                        "09/28",
+                        "222"
+                )
+        );
+        // generating pizza
+        Pizza pizza1 = new Pizza("Pizza A", 1000);
+        Pizza pizza2 = new Pizza("Pizza B", 2000);
+        Pizza pizza3 = new Pizza("Pizza C", 3000);
+        Pizza pizza4 = new Pizza("Pizza D", 4000);
+        // adding pizza to order
+        orderTotalIncorrect.setPizzasInOrder(new Pizza[]{pizza1, pizza2, pizza3, pizza4});
+        // setting total price
+        orderTotalIncorrect.setPriceTotalInPence(-1 + SystemConstants.ORDER_CHARGE_IN_PENCE);
+        // generating order validator
+        OrderValidator orderValidator = new OrderValidator();
+        // order validator returns an order with a defined order validation code
+        Order validatedOrder = orderValidator.validateOrder(orderTotalIncorrect, null);
+        assertEquals(OrderValidationCode.TOTAL_INCORRECT, validatedOrder.getOrderValidationCode());
+        // testing that total of all pizza costs + delivery fee = total price
 
     }
 
@@ -60,6 +171,10 @@ public class OrderValidatorTest
     }
 
     public void testingRestaurantClosed(){
+
+    }
+
+    public void testingGoodOrder(){
 
     }
 
